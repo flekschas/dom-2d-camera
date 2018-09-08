c0fe7a571bf97fd39b13bf2d38fddfc79fcf1669
Fritz Lekschas <service@lekschas.de>
2018-09-08 19:10:48 -0400

    Add `zoomSpeed`. Add `rotate` and `rotateSpeed` (a placeholder). Rename camera-2d. Expose getter setter for pan, zoom, rotate.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import createCamera from 'camera-2d';
+import createCamera from 'camera-2d-simple';
 import { vec3 } from 'gl-matrix';
 import createKeyPressed from 'key-pressed';
 import createMousePosition from 'mouse-position';
@@ -8,24 +8,31 @@ import createScroll from 'scroll-speed';
 const canvas2dCamera = (
   canvas,
   {
+    distance: initDistance = 1.0,
     pan: initPan = true,
     panSpeed: initPanSpeed = 1,
-    scale: initScale = true,
+    rotate: initRotate = true,
+    rotateSpeed: initRotateSpeed = 1,
     target: initTarget = [],
-    distance: initDistance = 1.0,
+    zoom: initZoom = true,
+    zoomSpeed: initZoomSpeed = 1,
   } = {},
 ) => {
   let pan = initPan;
   let panSpeed = initPanSpeed;
-  let scale = initScale;
-  let scroll = createScroll(canvas, scale);
-  let mousePressed = createMousePressed(canvas);
-  let mousePosition = createMousePosition(canvas);
+  let rotate = initRotate;
+  let rotateSpeed = initRotateSpeed;
+  let zoom = initZoom;
+  let zoomSpeed = initZoomSpeed;
+
   let camera = createCamera({ initTarget, initDistance });
   let isChanged = false;
+  let mousePosition = createMousePosition(canvas);
+  let mousePressed = createMousePressed(canvas);
+  let scroll = createScroll(canvas, zoom);
 
-  const getRelCoords = (x, y, w, h) => {
-    // Get relative webgl coordinates
+  const getGlPos = (x, y, w, h) => {
+    // Get relative WebGL position
     const relX = -1 + (x / w * 2);
     const relY = 1 + (y / h * -2);
     // Homogeneous vector
@@ -51,16 +58,16 @@ const canvas2dCamera = (
       isChanged = true;
     }
 
-    if (scale && scroll[1]) {
+    if (zoom && scroll[1]) {
       // Target == viewport center
       const { target, distance: oldDist } = camera;
-      const newDist = camera.distance * Math.exp(scroll[1] / height);
+      const newDist = zoomSpeed * camera.distance * Math.exp(scroll[1] / height);
 
       const dDist = newDist / oldDist;
       const recipDDist = 1 - dDist;
 
       // Get the relative WebGL coordinates of the mouse
-      const [relX, relY] = getRelCoords(
+      const [relX, relY] = getGlPos(
         mousePosition[0],
         mousePosition[1],
         width,
@@ -82,11 +89,11 @@ const canvas2dCamera = (
       isChanged = true;
     }
 
-    if (scale && (mousePressed.middle || (mousePressed.left && alt))) {
+    if (zoom && (mousePressed.middle || (mousePressed.left && alt))) {
       const d = mousePosition.y - mousePosition.prevY;
       if (!d) return undefined;
 
-      camera.distance *= Math.exp(d / height);
+      camera.distance *= zoomSpeed * Math.exp(d / height);
       isChanged = true;
     }
 
@@ -107,8 +114,33 @@ const canvas2dCamera = (
   };
 
   camera.tick = tick;
-  camera.getRelCoords = getRelCoords;
+  camera.getGlPos = getGlPos;
   camera.dispose = dispose;
+
+  Object.defineProperty(camera, 'isPan', {
+    get: () => pan,
+    set: (newPan) => { pan = !!newPan; },
+  });
+  Object.defineProperty(camera, 'panSpeed', {
+    get: () => panSpeed,
+    set: (newPanSpeed) => { panSpeed = +newPanSpeed > 0 ? +newPanSpeed : panSpeed; },
+  });
+  Object.defineProperty(camera, 'isRotate', {
+    get: () => rotate,
+    set: (newRotate) => { rotate = !!newRotate; },
+  });
+  Object.defineProperty(camera, 'rotateSpeed', {
+    get: () => rotateSpeed,
+    set: (newRotateSpeed) => { rotateSpeed = +newRotateSpeed > 0 ? +newRotateSpeed : rotateSpeed; },
+  });
+  Object.defineProperty(camera, 'isZoom', {
+    get: () => zoom,
+    set: (newZoom) => { zoom = !!newZoom; },
+  });
+  Object.defineProperty(camera, 'zoomSpeed', {
+    get: () => zoomSpeed,
+    set: (newZoomSpeed) => { zoomSpeed = +newZoomSpeed > 0 ? +newZoomSpeed : zoomSpeed; },
+  });
 
   return camera;
 };
