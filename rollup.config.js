31bc30cceb071f0c3758732a9f18799614df7778
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-02 21:49:03 -0500

    Log file size and visualize bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import buble from "@rollup/plugin-buble";
 import commonjs from "@rollup/plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+import visualizer from "rollup-plugin-visualizer";
 
 const config = (file, format, plugins = []) => ({
   input: "src/index.js",
@@ -18,7 +20,7 @@ const config = (file, format, plugins = []) => ({
 });
 
 export default [
-  config("dist/dom-2d-camera.js", "umd", [buble()]),
+  config("dist/dom-2d-camera.js", "umd", [buble(), visualizer(), filesize()]),
   config("dist/dom-2d-camera.min.js", "umd", [buble(), terser()]),
   config("dist/dom-2d-camera.esm.js", "esm")
 ];
